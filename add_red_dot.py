a9b65f84298753df952eec676bf9e91fe0b0944b
riduk17 <riduk17@gmail.com>
2019-10-14 16:27:56 +0200

    random generated dots on uploaded image

--- add_red_dot.py
+++ add_red_dot.py
@@ -1,25 +1,24 @@
 import random
-import math
 from PIL import Image, ImageDraw
+for i in range(10):
+    img = Image.open("./zdjecie.jpg")
+    img = img.convert("RGBA")
+    overlay = Image.new("RGBA", img.size, (0, 0, 0, 0))
+    draw = ImageDraw.Draw(overlay)
+    width, height = img.size
+    d = random.randint(10, 50)
+    x1 = random.randint(0, width - d)
+    x2 = x1 + d
+    y1 = random.randint(0, height - d)
+    y2 = y1 + d
+    r = random.randint(200, 255)
+    g = random.randint(0, 100)
+    b = random.randint(0, 100)
+    a = random.randint(50, 255)
+    color = (r, g, b, a)
+    draw.ellipse([(x1, y1), (x2, y2)], fill=color)
 
-img = Image.open("./img.png")
-img = img.convert("RGBA")
+    img = Image.alpha_composite(img, overlay)
+    img = img.convert("RGB")
 
-overlay = Image.new('RGBA', img.size, (0, 0, 0, 0))
-draw = ImageDraw.Draw(overlay)
-width, height = img.size
-d = math.floor(random.random() * 10 + 10)
-x1 = math.floor(random.random() * (width - d))
-x2 = x1 + d
-y1 = math.floor(random.random() * (height - d))
-y2 = y1 + d
-r = math.floor(random.random() * 200 + 100)
-g = math.floor(random.random() * 100 + 50)
-b = math.floor(random.random() * 100 + 50)
-a = math.floor(random.random() * 100 + 150)
-color = (r, g, b, a)
-draw.ellipse([(x1, y1), (x2, y2)], fill=color)
-
-img = Image.alpha_composite(img, overlay)
-
-img.save("./img2.png")
+    img.save("./wyjscie" + str(i) + ".jpg")
