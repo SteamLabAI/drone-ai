0dba9dfdec3ef5788f98b656526de69f7a1357ef
tmkch <tomekch6@gmail.com>
2019-10-16 18:21:41 +0200

    Improve dot finding algorithm

--- find_red_dot.py
+++ find_red_dot.py
@@ -5,12 +5,23 @@ for i in range(10):
     img = cv2.imread("wyjscie" + str(i) + ".jpg")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    lower_red = np.array([0, 50, 50])
-    upper_red = np.array([10, 255, 255])
-    mask = cv2.inRange(img, lower_red, upper_red)
-    mask = cv2.blur(mask, (20, 20))
+    mask_red_purple = cv2.inRange(  # For hue values (165, 180)
+        img,
+        np.array([165, 100, 100]),
+        np.array([180, 255, 255])
+    )
+    mask_red_orange = cv2.inRange(  # For hue values (0, 15)
+        img,
+        np.array([0, 100, 100]),
+        np.array([15, 255, 255])
+    )
 
-    th, threshed = cv2.threshold(
-        mask, 200, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    mask_red = mask_red_orange + mask_red_purple
+
+    # Blur the mask to ignore image noise and imperfections
+    mask_red = cv2.blur(mask_red, (10, 10))
+
+    # Discard values lower than 125
+    _, threshed = cv2.threshold(mask, 125, 255, cv2.THRESH_BINARY)
 
     cv2.imwrite("kropki" + str(i) + ".jpg", threshed)
