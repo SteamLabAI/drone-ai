f3d335cca7a1d72ab57700dc481cfb9de0023236
tmkch <tomekch6@gmail.com>
2019-10-16 18:17:01 +0200

    Tweak dot parameters

--- add_red_dot.py
+++ add_red_dot.py
@@ -6,15 +6,15 @@ for i in range(10):
     overlay = Image.new("RGBA", img.size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(overlay)
     width, height = img.size
-    d = random.randint(10, 50)
+    d = random.randint(15, 30)
     x1 = random.randint(0, width - d)
     x2 = x1 + d
     y1 = random.randint(0, height - d)
     y2 = y1 + d
-    r = random.randint(200, 255)
-    g = random.randint(0, 100)
-    b = random.randint(0, 100)
-    a = random.randint(50, 255)
+    r = random.randint(225, 255)
+    g = random.randint(0, 50)
+    b = random.randint(0, 50)
+    a = random.randint(100, 200)
     color = (r, g, b, a)
     draw.ellipse([(x1, y1), (x2, y2)], fill=color)
 
